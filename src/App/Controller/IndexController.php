7cfe8726ee8655ffddce4be1bf848c19a73acf2d
delboy1978uk <delboy1978uk@gmail.com>
2015-10-13 03:32:46 +0100

    adding cdn links. bone needs updating to pass vars to layout

--- src/App/Controller/IndexController.php
+++ src/App/Controller/IndexController.php
@@ -1,12 +1,25 @@
 <?php
 
 namespace App\Controller;
+
 use Bone\Mvc\Controller;
-use Bone\Mvc\Exception;
+use Del\Cdn;
 
 
 class IndexController extends Controller
 {
+    public function init()
+    {
+        // HTML Head variables
+        $this->view->css = Cdn::delCssLink();
+        $this->view->bscss = Cdn::bootstrapCssLink();
+        $this->view->uicss = Cdn::jQueryUiCssLink();
+        $this->view->jquery = Cdn::jQueryJavascript();
+        $this->view->jqueryui = Cdn::jQueryUIJavascript();
+        $this->view->bootstrap = Cdn::bootstrapJavascript();
+        $this->view->title = 'BTCTrader';
+    }
+
     public function indexAction()
     {
 
